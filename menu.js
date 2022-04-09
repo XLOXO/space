c3d3efc79cc150f858814973cbdb2cfa9ce6a70a
S Williams <woohoowilliams@gmail.com>
2022-04-09 14:18:56 +0000

    Credits

--- menu.js
+++ menu.js
@@ -124,31 +124,100 @@ scene("credits", (hiScore = 0) => {
     ]);
     add([
         text("Programming:"),
-        pos(240, 100),
+        pos(240, 60),
         color(255, 255, 128),
         origin("center"),
         scale(1),
     ]);
     add([
         text("swilliamsio"),
-        pos(240, 120),
+        pos(240, 80),
         color(255, 255, 128),
         origin("center"),
         scale(2),
     ]);
     add([
         text("https://www.swilliams.io/"),
-        pos(240, 140),
+        pos(240, 100),
+        color(255, 255, 128),
+        origin("center"),
+        area(),
+        scale(1),
+        "swilliamsio"
+    ]);
+    onClick("swilliamsio", () => window.open("https://www.swilliams.io/", '_blank'));
+    add([
+        text("Music:"),
+        pos(240, 120),
         color(255, 255, 128),
         origin("center"),
         scale(1),
-        "button",
-        {
-            clickAction: () => {
-                window.open("https://www.swilliams.io/", '_blank');
-            }
-        },
     ]);
+    add([
+        text("TeraVex"),
+        pos(240, 140),
+        color(255, 255, 128),
+        origin("center"),
+        scale(2),
+    ]);
+    add([
+        text("https://www.newgrounds.com/audio/listen/997121"),
+        pos(240, 160),
+        color(255, 255, 128),
+        origin("center"),
+        area(),
+        scale(1),
+        "TeraVex"
+    ]);
+    onClick("TeraVex", () => window.open("https://www.newgrounds.com/audio/listen/997121", '_blank'));
+    add([
+        text("SFX:"),
+        pos(240, 180),
+        color(255, 255, 128),
+        origin("center"),
+        scale(1),
+    ]);
+    add([
+        text("SwissArcadeGameEntertainment"),
+        pos(240, 200),
+        color(255, 255, 128),
+        origin("center"),
+        scale(2),
+    ]);
+    add([
+        text("https://opengameart.org/content/sfx-the-ultimate-2017-16-bit-mini-pack"),
+        pos(240, 220),
+        color(255, 255, 128),
+        origin("center"),
+        area(),
+        scale(1),
+        "SwissArcadeGameEntertainment"
+    ]);
+    onClick("SwissArcadeGameEntertainment", () => window.open("https://opengameart.org/content/sfx-the-ultimate-2017-16-bit-mini-pack", '_blank'));
+    add([
+        text("SFX:"),
+        pos(240, 240),
+        color(255, 255, 128),
+        origin("center"),
+        scale(1),
+    ]);
+    add([
+        text("Kenny"),
+        pos(240, 260),
+        color(255, 255, 128),
+        origin("center"),
+        scale(2),
+    ]);
+    add([
+        text("https://kenney.nl"),
+        pos(240, 280),
+        color(255, 255, 128),
+        origin("center"),
+        area(),
+        scale(1),
+        "kenney"
+    ]);
+    onClick("kenney", () => window.open("https://kenney.nl", '_blank'));
     add([
         text("Back"),
         pos(240, height() - 30),
